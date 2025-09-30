f7c563a02cb7f2e84c1708b63e02a39edac35ac0
Ashu123falle <ashutoshfalle28@gmail.com>
2025-09-30 15:50:04 +0530

    Update axiosInstance.js

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: "https://localhost:7096/api", 
-    baseURL: "https://expense-tracker-ybyq.onrender.com/api", 
+    // baseURL: "https://expense-tracker-ybyq.onrender.com/api", 
     baseURL: "https://expense-tracker-zgt8.onrender.com/api",
     
     headers: {
